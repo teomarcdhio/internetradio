d0d5768df4e9e18ed00233a5e7486f038fbf613e
Matteo <matteo@x240>
2017-03-12 21:45:04 +0000

    Piping track

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const Parser = require('icecast-parser');
 const fs = require('fs');
+const http = require('http');
 var stationurl = "http://216.126.195.37:8078/stream";
 var track = fs.createWriteStream('myOutput.mp3');
 const radioStation = new Parser({url: stationurl,
-                                keepListen: false,
+                                keepListen: true,
                                 metadataInterval: 1
                                 });
 
 radioStation.on('metadata', function(metadata) {
     console.log(metadata.StreamTitle);
 });
+
+radioStation.on('stream', function(stream) {
+    stream.pipe(track);
+});
+
+//http.get( stationurl
+//, (res) => {
+//  res.pipe(track);
+//});
