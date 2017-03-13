b2d6dd355d0741f8d6027975746b9216ffcbf316
Matteo Marcolini <matteo@MMarcolini700-DT.localdomain>
2017-03-13 16:11:44 +0000

    Some progress

--- index.js
+++ index.js
@@ -2,19 +2,35 @@ const Parser = require('icecast-parser');
 const fs = require('fs');
 const http = require('http');
 var stationurl = "http://216.126.195.37:8078/stream";
-var track = fs.createWriteStream('files/myOutput.mp3');
+
 const radioStation = new Parser({url: stationurl,
-                                keepListen: true,
+                                keepListen: false,
                                 metadataInterval: 1
                                 });
 
+var title = 'First' ;
 radioStation.on('metadata', function(metadata) {
-    console.log(metadata.StreamTitle);
+
+
+    if ( title !== metadata.StreamTitle) {
+      title = metadata.StreamTitle;
+      var path = 'files/';
+      var ext = '.mp3';
+      var location = path + title + ext;
+      console.log(location);
+      var track = fs.createWriteStream(location);
+      console.log(title);
+      http.get( stationurl
+      , (res) => {
+        res.pipe(track);
+      });
+    }
+
 });
 
-radioStation.on('stream', function(stream) {
-    stream.pipe(track);
-});
+//radioStation.on('stream', function(stream) {
+//    stream.pipe(track);
+//});
 
 //http.get( stationurl
 //, (res) => {
