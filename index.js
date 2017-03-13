8c2a090e6f6e7312e8507a55056f908ea05ae8c9
Matteo Marcolini <matteo@MMarcolini700-DT.localdomain>
2017-03-13 14:03:55 +0000

    added gitignore

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Parser = require('icecast-parser');
 const fs = require('fs');
 const http = require('http');
 var stationurl = "http://216.126.195.37:8078/stream";
-var track = fs.createWriteStream('myOutput.mp3');
+var track = fs.createWriteStream('files/myOutput.mp3');
 const radioStation = new Parser({url: stationurl,
                                 keepListen: true,
                                 metadataInterval: 1
