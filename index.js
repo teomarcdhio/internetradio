0ceabf7275b0bcba4228a9a300d2cb1e607b53c8
Matteo Marcolini <matteo@MMarcolini700-DT.localdomain>
2017-03-14 15:36:19 +0000

    Working version

--- index.js
+++ index.js
@@ -4,29 +4,34 @@ const http = require('http');
 var stationurl = "http://216.126.195.37:8078/stream";
 
 const radioStation = new Parser({url: stationurl,
-                                keepListen: false,
-                                metadataInterval: 1
-                                });
+  keepListen: false,
+  metadataInterval: 1
+});
 
+var reout ;
 var title = 'First' ;
 radioStation.on('metadata', function(metadata) {
-
-
-    if ( title !== metadata.StreamTitle) {
-      title = metadata.StreamTitle;
-      var path = 'files/';
-      var ext = '.mp3';
-      var location = path + title + ext;
-      console.log(location);
-      var track = fs.createWriteStream(location);
-      console.log(title);
-      http.get( stationurl
-      , (res) => {
-        res.pipe(track);
-      });
+  if ( title !== metadata.StreamTitle) {
+    if(reout){
+      reout.destroy();
     }
+    title = metadata.StreamTitle;
+    var path = 'files/';
+    var ext = '.mp3';
+    var location = path + title + ext;
+    console.log(location);
+    var track = fs.createWriteStream(location);
+    console.log(title);
+    var req = http.get( stationurl
+      , (res) => {
+        res.pipe(track); // this adds 16 seconds at the end ; possibly cache
+      });
+      reout = req;
 
-});
+
+    };
+
+  });
 
 //radioStation.on('stream', function(stream) {
 //    stream.pipe(track);
@@ -36,3 +41,30 @@ radioStation.on('metadata', function(metadata) {
 //, (res) => {
 //  res.pipe(track);
 //});
+/*
+var reout;
+var title = 'First' ;
+radioStation.on('metadata', function(metadata) {
+  if ( title !== metadata.StreamTitle) {
+    title = metadata.StreamTitle;
+    var path = 'files/';
+    var ext = '.mp3';
+    var location = path + title + ext;
+    console.log(location);
+    var track = fs.createWriteStream(location);
+    console.log(title);
+    var options = {
+    port: 8078,
+    hostname: '216.126.195.37',
+    method: 'CONNECT',
+    path: '/stram'
+    };
+    var req = http.request(options);
+    req.on('connect', (res) => {
+      res.pipe(track);
+    });
+    reout = req;
+
+}
+});
+*/
